f9ed01d193ac508636ba0f73704c1641dac13d5c
JuanVel23 <jsebastianvel23@gmail.com>
2025-10-07 22:50:57 -0500

    Incio Proyecto

---
+++ src/main/java/com/siontrack/siontrack/SiontrackApplication.java
@@ -0,0 +1,13 @@
+package com.siontrack.siontrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiontrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiontrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siontrack/siontrack/SiontrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siontrack.siontrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiontrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
